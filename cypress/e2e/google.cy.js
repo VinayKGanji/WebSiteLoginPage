45cff1c8a7ae8b60d5403a880d79f18ba69bd9a1
Vinay Ganji <vinayganji@Vinays-MacBook-Pro.local>
2023-01-07 14:58:27 +0530

    google home

---
+++ cypress/e2e/google.cy.js
@@ -0,0 +1,19 @@
+///<reference types ="cypress"/>
+
+describe('Gmail login', ()=>{
+
+it ('Gmail login', ()=>{
+
+    cy.visit('https://www.google.co.in/')
+    cy.get('.gLFyf').type('gmail{enter}')
+    
+    
+
+    
+
+
+
+
+
+})
+})
