c2f20a3f8211bd550c782bc3a5e0c394aa5af20b
Vinay Ganji <vinayganji@Vinays-MacBook-Pro.local>
2023-01-07 22:10:48 +0530

    last commit

--- cypress/e2e/google.cy.js
+++ cypress/e2e/google.cy.js
@@ -4,19 +4,7 @@ describe('Gmail login', ()=>{
 
 it ('Gmail login', ()=>{
 
-<<<<<<< HEAD
     cy.visit('https://www.google.co.in/')
     cy.get('.gLFyf').type('gmail{enter}')
-    
-    
-
-    
-
-=======
->>>>>>> mytest
-
-
-
-
 })
 })
